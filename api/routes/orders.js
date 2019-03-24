f0ca3016f1e874bedba49142430ef096e2d205fa
amin04 <aminmithun1992@gmail.com>
2019-03-24 09:40:09 +0600

    orders post route added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,24 +1,40 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 
 router.get('/', (req, res) => {
-    res.status(200).json({
-        message: "get orders"
-    })
+    Order.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.post('/', (req, res) => {
-    const order = {
-        productId : req.body.productId,
-        quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message: "Order was Created",
-        order: order
-
-    })
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product : req.body.productId
+    });
+    order
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.patch('/:productId', (req, res) => {
