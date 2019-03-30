2d82af5b31f90341efb7231f8ed1fa9606680461
amin04 <aminmithun1992@gmail.com>
2019-03-31 00:32:28 +0600

    json web token and login route added -- video 12 completed

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -73,8 +75,44 @@ router.post('/signup', (req, res, next) => {
     })
 })
 
-router.delete('/userId', (req, res, next) => {
-    User.deleteOne({_id: req.params.userId})
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .exec()
+        .then(user => {
+            console.log(user);
+            if(user.length < 1) {
+                return res.status(401).json({message: 'Auth failed'});
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if(err) {
+                    return res.status(401).json({message: 'Auth failed'});
+                }
+                if(result) {
+
+                    const token = jwt.sign(
+                        {
+                            email: user.email,
+                            userId: user._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+
+                    return res.status(200).json({
+                        message: 'Auth sucessful',
+                        token: token
+                    });
+                }
+                res.status(401).json({message: 'Auth failed'});
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 router.delete('/:userId', (req, res, next) => {
